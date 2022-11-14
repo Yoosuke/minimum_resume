4583c48646ab4972d1308c9a93afd07ac6261590
Yoosuke <yoosukenakao@thewaggle.co.jp>
2022-11-14 21:31:57 +0900

    def school adding & career format adding

--- lib/minimum_resume.ex
+++ lib/minimum_resume.ex
@@ -1,18 +1,87 @@
 defmodule MinimumResume do
   @moduledoc """
   Documentation for `MinimumResume`.
+  コンセプト：最小限のインプットで履歴書作成
+  TODO
+  * 定型文パターンを作成（学歴：school）OK
+  * 定型文パターンを作成（経歴：career）
+  * 定型文パターンを作成（業務経験：business experience）
+  * 定型文パターンを作成（資格：certification）
+  * 定型文パターンを作成（技術：skill）
+  * 定型文パターンを作成 (自己PR：summary)
   """
 
   @doc """
-  Hello world.
+  学歴のフォーマットを出力
 
   ## Examples
 
-      iex> MinimumResume.hello()
-      :world
+      iex> MinimumResume.school("19780415")
+      "1997/03 〇〇高等学校 卒業\n1997/04 〇〇大学/専門学校/短期大学 入学\n1999/03 専門学校/短期大学 卒業\n2001/03 〇〇大学 卒業"
 
   """
-  def hello do
-    :world
+  def school(string) do
+    add = fn str, day -> str <> day end
+    #比較元を作成
+    input = string
+      |> String.slice(0..3)
+      |> add.("0402")
+
+    output = string
+      |> String.slice(0..3)
+      |> add.("1231")
+
+
+    #比較する
+    nendo =
+      if input <= string and string <= output do
+        String.slice(string, 0..3) |> String.to_integer()
+      else
+        ( String.slice(string, 0..3) |> String.to_integer() ) - 1
+      end
+
+    #出力
+
+    "#{nendo + 19}/03 〇〇高等学校 卒業\n#{nendo + 19}/04 〇〇大学/専門学校/短期大学 入学\n#{nendo + 21}/03 専門学校/短期大学 卒業\n#{nendo + 23}/03 〇〇大学 卒業"
   end
+
+  @doc """
+  経歴のフォーマットを出力
+
+  ## Examples
+
+      iex> MinimumResume.career("199704", 4)
+      "1997/04 ~ 2001/03"
+      iex> MinimumResume.career("199704", "200109")
+      "1997/04 ~ 2001/09"
+      iex> MinimumResume.career("199704")
+      "1997/04 ~ 現在"
+  """
+
+  def career(str, year) when is_binary(str) and is_integer(year) do
+    "1997/04 ~ 2001/03"
+  end
+
+  def career(str1, str2) when is_binary(str1) and is_binary(str2) do
+    "1997/04 ~ 2001/09"
+  end
+
+  def career(str) when is_binary(str) do
+    "1997/04 ~ 現在"
+  end
+
+
+  @doc """
+  経歴のフォーマット達を出力
+
+  ## Examples
+
+      iex> MinimumResume.careers([{"199704", 4}, {"199704, 200109}, "199704"])
+      "1997/04 ~ 2001/03\n1997/04 ~ 2001/09\n1997/04 ~ 現在"
+  """
+  def careers(list) when is_list(list) do
+    "1997/04 ~ 2001/03\n1997/04 ~ 2001/09\n1997/04 ~ 現在"
+  end
+
+
 end
--- test/minimum_resume_test.exs
+++ test/minimum_resume_test.exs
@@ -1,8 +1,17 @@
 defmodule MinimumResumeTest do
   use ExUnit.Case
-  doctest MinimumResume
+  #doctest MinimumResume
 
-  test "greets the world" do
-    assert MinimumResume.hello() == :world
+  test "入学・卒業年度出力" do
+    assert MinimumResume.school("19780415") == "1997/03 〇〇高等学校 卒業\n1997/04 〇〇大学/専門学校/短期大学 入学\n1999/03 専門学校/短期大学 卒業\n2001/03 〇〇大学 卒業"
   end
+
+  test "入学・卒業年度出力 境界値0401" do
+    assert MinimumResume.school("19780401") == "1996/03 〇〇高等学校 卒業\n1996/04 〇〇大学/専門学校/短期大学 入学\n1998/03 専門学校/短期大学 卒業\n2000/03 〇〇大学 卒業"
+  end
+
+  test "入学・卒業年度出力 境界値0402" do
+    assert MinimumResume.school("19780402") == "1997/03 〇〇高等学校 卒業\n1997/04 〇〇大学/専門学校/短期大学 入学\n1999/03 専門学校/短期大学 卒業\n2001/03 〇〇大学 卒業"
+  end
+
 end
